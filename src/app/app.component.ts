a1f2944bdfe3fea8a06c3d222607fd6e399c0939
suhao <570498204@qq.com>
2023-05-31 13:22:47 +0800

    :tada: angularJs devui-admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
+import { DEFAULT_LANG, I18N_LANGUAGES } from '../config/language-config';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: 'da-app',
+  template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {
-  title = 'my-devui-admin';
+  currentLang = localStorage.getItem('lang') || window.navigator.language.toLowerCase() || DEFAULT_LANG;
+
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(I18N_LANGUAGES);
+    translate.setDefaultLang(DEFAULT_LANG);
+    translate.use(this.currentLang);
+  }
 }
